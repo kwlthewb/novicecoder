d7b21c15767b25bede7b0589faef349421350b93
Bill Li <kitwing.li@gmail.com>
2016-03-14 21:12:21 -0400

    more examples

---
+++ workspace_c++/Concurrency/Semaphores.h
@@ -0,0 +1,18 @@
+/*
+ * Semaphores.h
+ *
+ *  Created on: Mar 14, 2016
+ *      Author: bill
+ */
+
+#ifndef SEMAPHORES_H_
+#define SEMAPHORES_H_
+#include "Incls.h"
+
+class Semaphores {
+public:
+	Semaphores();
+	virtual ~Semaphores();
+};
+
+#endif /* SEMAPHORES_H_ */
